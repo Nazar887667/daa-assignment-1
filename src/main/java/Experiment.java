5f4344acadd664e38e0f29b7c1c934273ac86bf9
Nazar <kakentai804@gmail.com>
2026-09-20 22:33:12 +0500

    feat(experiment): test multiple input types

--- src/main/java/Experiment.java
+++ src/main/java/Experiment.java
@@ -12,14 +12,19 @@ public class Experiment {
             System.out.println();
             System.out.println("Input size: " + n);
 
-            runExperiment("Random", InputGenerator.randomArray(n));
-            runExperiment("Sorted", InputGenerator.sortedArray(n));
-            runExperiment("Reverse-sorted", InputGenerator.reverseSortedArray(n));
-            runExperiment("Duplicate-heavy", InputGenerator.duplicateHeavyArray(n));
+            runMergeSortExperiment("Random", InputGenerator.randomArray(n));
+            runMergeSortExperiment("Sorted", InputGenerator.sortedArray(n));
+            runMergeSortExperiment("Reverse-sorted", InputGenerator.reverseSortedArray(n));
+            runMergeSortExperiment("Duplicate-heavy", InputGenerator.duplicateHeavyArray(n));
+
+            runQuickSortExperiment("Random", InputGenerator.randomArray(n));
+            runQuickSortExperiment("Sorted", InputGenerator.sortedArray(n));
+            runQuickSortExperiment("Reverse-sorted", InputGenerator.reverseSortedArray(n));
+            runQuickSortExperiment("Duplicate-heavy", InputGenerator.duplicateHeavyArray(n));
         }
     }
 
-    private static void runExperiment(String type, int[] array) {
+    private static void runMergeSortExperiment(String type, int[] array) {
 
         MergeSorter sorter = new MergeSorter();
 
@@ -35,10 +40,37 @@ public class Experiment {
         long comparisons = sorter.getComparisons();
 
         System.out.println(
-                type +
+                "Merge Sort - " +
+                        type +
                         ": time = " + executionTime + " ns" +
                         ", recursion depth = " + recursionDepth +
                         ", comparisons = " + comparisons
+        );
+    }
+
+    private static void runQuickSortExperiment(String type, int[] array) {
+
+        QuickSorter sorter = new QuickSorter();
+
+        long startTime = System.nanoTime();
+
+        sorter.sort(array);
+
+        long endTime = System.nanoTime();
+
+        long executionTime = endTime - startTime;
+
+        int recursionDepth = sorter.getMaxRecursionDepth();
+        long comparisons = sorter.getComparisons();
+        long swaps = sorter.getSwaps();
+
+        System.out.println(
+                "Quick Sort - " +
+                        type +
+                        ": time = " + executionTime + " ns" +
+                        ", recursion depth = " + recursionDepth +
+                        ", comparisons = " + comparisons +
+                        ", swaps = " + swaps
         );
     }
 }
