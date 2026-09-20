fb99aff0c81db97f2ac84c83cc432cf813bbb75d
Nazar <kakentai804@gmail.com>
2026-09-20 22:31:44 +0500

    feat(experiment): test multiple input types

--- src/main/java/Experiment.java
+++ src/main/java/Experiment.java
@@ -2,34 +2,43 @@ public class Experiment {
 
     public static void main(String[] args) {
 
-        System.out.println("DAA Execution Time Experiment");
-        System.out.println("=============================");
+        System.out.println("DAA Experimental Results");
+        System.out.println("========================");
 
         int[] sizes = {100, 1000, 5000};
 
         for (int n : sizes) {
 
-            int[] array = InputGenerator.randomArray(n);
+            System.out.println();
+            System.out.println("Input size: " + n);
 
-            MergeSorter sorter = new MergeSorter();
-
-            long startTime = System.nanoTime();
-
-            sorter.sort(array);
-
-            long endTime = System.nanoTime();
-
-            long executionTime = endTime - startTime;
-
-            int recursionDepth = sorter.getMaxRecursionDepth();
-            long comparisons = sorter.getComparisons();
-
-            System.out.println(
-                    "n = " + n +
-                            ", time = " + executionTime + " ns" +
-                            ", recursion depth = " + recursionDepth +
-                            ", comparisons = " + comparisons
-            );
+            runExperiment("Random", InputGenerator.randomArray(n));
+            runExperiment("Sorted", InputGenerator.sortedArray(n));
+            runExperiment("Reverse-sorted", InputGenerator.reverseSortedArray(n));
+            runExperiment("Duplicate-heavy", InputGenerator.duplicateHeavyArray(n));
         }
+    }
+
+    private static void runExperiment(String type, int[] array) {
+
+        MergeSorter sorter = new MergeSorter();
+
+        long startTime = System.nanoTime();
+
+        sorter.sort(array);
+
+        long endTime = System.nanoTime();
+
+        long executionTime = endTime - startTime;
+
+        int recursionDepth = sorter.getMaxRecursionDepth();
+        long comparisons = sorter.getComparisons();
+
+        System.out.println(
+                type +
+                        ": time = " + executionTime + " ns" +
+                        ", recursion depth = " + recursionDepth +
+                        ", comparisons = " + comparisons
+        );
     }
 }
